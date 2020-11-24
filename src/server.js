5442eb0a5ffb98b460feff517cd96d196f267820
Claudio.Carrizo <claudio.carrizo@bitlogic.io>
2020-11-24 08:23:56 -0300

    update server.js

--- src/server.js
+++ src/server.js
@@ -3,9 +3,10 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname+'/dist/ng-blog'));
-app.get('/',function(req,res){
-    res.sendFile(path.join(__dirname+'/dist/ng-blog/index.html'));
-});
+app.use(express.static(__dirname+'/dist/mi-administrador-front'));
+app.get('/*', function(req, res) {
+    res.sendFile('index.html', {root: 'dist/mi-administrador-front/'}
+  );
+  });
 
 app.listen(process.env.PORT || 8080);

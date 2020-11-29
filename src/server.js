3cb6e0dc5c0c3c64c1f0ef5323dacaef497b1e61
Claudio.Carrizo <claudio.carrizo@bitlogic.io>
2020-11-29 11:33:52 -0300

    update

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname + '/dist/mi-administrador-front'));
+app.use(express.static(__dirname + '/'));
 
 app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname + '/index.html')

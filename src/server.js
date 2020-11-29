5bfde0de83917f94902aea7d98175a753417960d
Claudio.Carrizo <claudio.carrizo@bitlogic.io>
2020-11-28 21:33:43 -0300

    update server.js

--- src/server.js
+++ src/server.js
@@ -1,12 +1,16 @@
+//Install express server
 const express = require('express');
 const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname+'/dist/mi-administrador-front'));
-app.get('/*', function(req, res) {
-    res.sendFile('index.html', {root: 'dist/mi-administrador-front/'}
-  );
-  });
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/mi-administrador-front'));
 
-app.listen(process.env.PORT || 4200);
+app.get('/*', function(req,res) {
+    
+res.sendFile(path.join(__dirname+'/dist/mi-administrador-front/index.html'));
+});
+
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);

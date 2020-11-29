c8bed39281db53127a6c436209824fac77c4b836
Claudio.Carrizo <claudio.carrizo@bitlogic.io>
2020-11-29 17:10:20 -0300

    update

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname + '/index.html'));
+app.use(express.static(__dirname + '/dist/mi-administrador-front'));
 
 app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname + '/index.html')
+  res.sendFile(path.join(__dirname + '/dist/mi-administrador-front/index.html')
   );
 });
 
